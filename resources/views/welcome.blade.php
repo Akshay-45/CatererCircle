259a2c33dea11d64d0ebb82b41776cb5782890c2
akshaychatim <aky.chatim@gmail.com>
2020-03-17 15:41:03 +0530

    Merge test on other client

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,8 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+
+
         <!-- Styles -->
         <style>
             html, body {
